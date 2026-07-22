24ebcfa7c9136f5426afecb7689306f16bd48ba1
WWGJ0 <2701748990@qq.com>
2026-07-22 23:51:57 +0800

    today 1

--- main.cpp
+++ main.cpp
@@ -174,7 +174,23 @@ public:
         return min(minDepth(root->left), minDepth(root->right)) +1;
     }
 };
-int main() {
 
+#二叉树的翻转
+class Solution11 {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+      if (root == nullptr)
+      {
+            return nullptr;
+        }
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+ };
+
+int main() {
     return 0;
 }
