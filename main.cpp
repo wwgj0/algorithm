6885aad08e30b170f4ab3ccdb5ec8b1757368f96
WWGJ0 <2701748990@qq.com>
2026-07-24 22:54:36 +0800

    Add sortedArrayToBST

--- main.cpp
+++ main.cpp
@@ -245,6 +245,25 @@ public:
     }
 };
 
+#有序数组转换为二叉搜索树
+class Solution14 {
+public:
+    TreeNode* helper(vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+        int mid = (left + right) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = helper(nums, left, mid - 1);
+        root->right = helper(nums, mid + 1, right);
+        return root;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+};
+
 int main() {
     return 0;
 }
