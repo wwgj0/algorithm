7c8dfa8b8a53cef7393ab876da38e14f092d82c8
WWGJ0 <2701748990@qq.com>
2026-07-25 22:20:24 +0800

    Add isValid parentheses

--- main.cpp
+++ main.cpp
@@ -293,6 +293,33 @@ public:
     }
 };
 
+#有效的括号
+class Solution16 {
+public:
+    bool isValid(string s) {
+        if (s.size() % 2 != 0)
+        return false;
+        stack<char> st;
+
+        for (char c : s)
+        {
+            if (c == '(')
+            st.push(')');
+            else if (c == '[')
+            st.push(']');
+            else if (c == '{')
+            st.push('}');
+            else
+            {
+                if (st.empty() || st.top() != c)
+                    return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+
 int main()
 {
 
