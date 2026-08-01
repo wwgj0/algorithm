88d19580068ffaa27f7f59d4fc97feb0afb2b349
WWGJ0 <2701748990@qq.com>
2026-08-01 22:18:24 +0800

    today 2

--- main.cpp
+++ main.cpp
@@ -439,6 +439,69 @@ public:
         return maxn;
     }
 };
+
+#盛最多水的容器
+class Solution23 {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int Max = 0;
+        while (left < right)
+        {
+            int width = right - left;
+            int h = min(height[left], height[right]);
+            Max = max(Max, width * h);
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return Max;
+    }
+};
+
+#三数之和
+class Solution24 {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            if (nums[i] > 0)
+                break;
+
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0)
+                {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+                    left++;
+                    right--;
+                } else if (sum < 0)
+                {
+                    left++;
+                } else
+                {
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+};
 int main()
 {
 
