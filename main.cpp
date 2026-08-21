c8b2f24beb682ff4681b3f90a4a738ff5e1ae63d
WWGJ0 <2701748990@qq.com>
2026-08-21 20:45:02 +0800

    完善readme: 添加搜索二维矩阵II

--- main.cpp
+++ main.cpp
@@ -855,6 +855,30 @@ public:
         }
     }
 };
+
+#搜索二维矩阵 II
+class Solution39 {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int left = 0, right = n * m - 1;
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            int row = mid / m;
+            int col = mid % m;
+            int val = matrix[row][col];
+            if(val == target)
+                return true;
+            else if(val < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+};
 int main()
 {
     return 0;
